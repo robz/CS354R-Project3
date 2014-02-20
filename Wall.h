2ee138f68550059cd463485cc848670e699615fa
Steven Wilken <swilken@cs.utexas.edu>
2014-02-20 17:15:34 -0600

    Add missing file

---
+++ Wall.h
@@ -0,0 +1,26 @@
+#ifndef __Wall_h_
+#define __Wall_h_
+
+#include "GameObject.h"
+#include <OgreEntity.h>
+#include <OgreSceneManager.h>
+#include "Simulator.h"
+#include <Ogre.h>
+
+class Wall : public GameObject {
+	public:
+		Wall(Ogre::String, 
+			Ogre::SceneManager*, 
+			Simulator*, 
+			Ogre::Real, 
+			Ogre::Real, 
+			Ogre::Real, 
+			Ogre::Real, 
+			Ogre::Real, 
+			Ogre::Vector3,
+			Ogre::Vector3,
+			Ogre::String
+			);
+};
+
+#endif
